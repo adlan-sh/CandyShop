732b14b68f7f7a099bdaec3d247633b06c4b5e95
Адлан Шамавов <a.shamavov@iqdev.digital>
2024-05-25 17:26:07 +0500

    Поправил фронт

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,14 +9,9 @@ import Payment from './payment';
 function App() {
   return (
     <>
-<<<<<<< HEAD
-      { <Header />
-      <Main />
-=======
       <Header />
->>>>>>> 58b7bd8b69b491a4e968fb0fe9bbbecfa773dd98
       <Footer />
-      <Modal />}
+      <Modal />
     </>
   );
 }
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -5,6 +5,7 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Root from './routes/Root';
 import Catalog from './routes/Catalog';
 import Layout from './routes/Layout';
+import App from './App';
 import './App.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import axios from 'axios';
